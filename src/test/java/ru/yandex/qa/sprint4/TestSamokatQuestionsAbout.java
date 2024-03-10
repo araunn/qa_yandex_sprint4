af14f32bdcd5caa7adcf787537f31bb8820c4ccf
araunn <75637098+araunn@users.noreply.github.com>
2024-03-11 00:14:52 +0200

    move locators to pageobject and change package

--- src/test/java/ru/yandex/qa/sprint4/TestSamokatQuestionsAbout.java
+++ src/test/java/ru/yandex/qa/sprint4/TestSamokatQuestionsAbout.java
@@ -9,9 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
-import ru.yandex.qa.sprint4.pageobject.MainPageQuestionAbout;
+import ru.yandex.data.AnswerMessageData;
+import ru.yandex.pageobject.MainPageQuestionAbout;
 
 @RunWith(Parameterized.class)
 public class TestSamokatQuestionsAbout {
@@ -19,7 +18,6 @@ public class TestSamokatQuestionsAbout {
 	private final By question;
 	private final By answer;
 	private final String massage;
-
 	
 	public TestSamokatQuestionsAbout(By question, By answer, String massage) {
 		this.question = question;
@@ -31,19 +29,18 @@ public class TestSamokatQuestionsAbout {
 	public void teardown() {
 	    driver.quit();
 	}
-
-			
+		
 		@Parameterized.Parameters
 		public static Object[][] getQuestionAnswer() {
 		   return new Object[][] {
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_1),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_1),LocatorAndTestData.ANSWER_MESSAGE_1},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_2),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_2),LocatorAndTestData.ANSWER_MESSAGE_2},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_3),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_3),LocatorAndTestData.ANSWER_MESSAGE_3},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_4),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_4),LocatorAndTestData.ANSWER_MESSAGE_4},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_5),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_5),LocatorAndTestData.ANSWER_MESSAGE_5},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_6),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_6),LocatorAndTestData.ANSWER_MESSAGE_6},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_7),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_7),LocatorAndTestData.ANSWER_MESSAGE_7},
-		           {By.id(LocatorAndTestData.LOCATOR_QUESTION_8),By.xpath(LocatorAndTestData.LOCATOR_ANSWER_8),LocatorAndTestData.ANSWER_MESSAGE_8},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_1,MainPageQuestionAbout.LOCATOR_ANSWER_1,AnswerMessageData.ANSWER_MESSAGE_1},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_2,MainPageQuestionAbout.LOCATOR_ANSWER_2,AnswerMessageData.ANSWER_MESSAGE_2},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_3,MainPageQuestionAbout.LOCATOR_ANSWER_3,AnswerMessageData.ANSWER_MESSAGE_3},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_4,MainPageQuestionAbout.LOCATOR_ANSWER_4,AnswerMessageData.ANSWER_MESSAGE_4},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_5,MainPageQuestionAbout.LOCATOR_ANSWER_5,AnswerMessageData.ANSWER_MESSAGE_5},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_6,MainPageQuestionAbout.LOCATOR_ANSWER_6,AnswerMessageData.ANSWER_MESSAGE_6},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_7,MainPageQuestionAbout.LOCATOR_ANSWER_7,AnswerMessageData.ANSWER_MESSAGE_7},
+		           {MainPageQuestionAbout.LOCATOR_QUESTION_8,MainPageQuestionAbout.LOCATOR_ANSWER_8,AnswerMessageData.ANSWER_MESSAGE_8},
 		   };
 		}
 		
@@ -51,7 +48,7 @@ public class TestSamokatQuestionsAbout {
 		@Test
 		public void testQuestionsAboutCrome() {
 			driver = new ChromeDriver();
-			driver.get(LocatorAndTestData.SITE_ADRESS);
+			driver.get(MainPageQuestionAbout.SITE_ADRESS);
 			MainPageQuestionAbout page = new MainPageQuestionAbout(driver);
 			page.aceptCookie();
 			page.clickButton(question);
@@ -62,7 +59,7 @@ public class TestSamokatQuestionsAbout {
 		@Test
 		public void testQuestionsAboutFireFox() {
 			driver = new FirefoxDriver();
-			driver.get(LocatorAndTestData.SITE_ADRESS);
+			driver.get(MainPageQuestionAbout.SITE_ADRESS);
 			MainPageQuestionAbout page = new MainPageQuestionAbout(driver);
 			page.aceptCookie();
             page.clickButton(question);
