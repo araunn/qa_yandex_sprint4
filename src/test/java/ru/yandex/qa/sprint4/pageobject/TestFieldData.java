4200f6fe3abca3d7e87de11ba4f317310146054f
Vasiliy Stulikov <vstulikov@mail.ru>
2024-03-11 11:32:18 +0200

    change package and locators

--- src/test/java/ru/yandex/qa/sprint4/pageobject/TestFieldData.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.qa.sprint4.pageobject;
-
-public class TestFieldData {
-  static final String NAME = "иван";
-  static final String SURNAME = "иванов";
-  static final String ADRESS = "ул Усвчева5";
-  static final String TELEPHONE = "89037854144";
-  static final String COMMENT = "Мой комментарий";
-}
